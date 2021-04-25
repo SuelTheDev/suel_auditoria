9466a02fb002ef72bfa9b7de3dbab8d9976c63a3
Archnael Santos <home.win32@gmail.com>
2021-04-25 04:07:16 -0300

    organização e criação da nova feature de buscar informações sobre a fac

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,5 +7,5 @@ description 'Script para ver informações dos players através de webhook pelo 
 version '1.0.0'
 
 server_only 'yes'
-server_scripts {'@vrp/lib/utils.lua','lucifer.lua'}
-discord_webhook 'SEU WEBHOOK AQUI'
+server_scripts {'@vrp/lib/utils.lua','lucifer.lua', 'l2.lua'}
+discord_webhook 'https://discord.com/api/webhooks/835655302493634590/9DF542yQfG24phgCC9Bzs4_VxQbzS-SGaSyCSW4vHboH_AxeoFVnuOWQrr_fA2d612Va'
